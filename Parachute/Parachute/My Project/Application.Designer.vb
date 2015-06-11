e30a3e01384e922101a3d026260d3c22b2acbdcf
OtherBarry <charlie.wilson4@gmail.com>
2015-06-11 16:49:57 +1000

    Add Simple Screen with Movement
    * Added Screen
    * Added Character Art (Small)
    * Added Colour Scheme
    * Added Scroll Ticker
    * Added Left-Right Movement (Responds to Arrow Keys and WASD)
    * Added  Screen Edge Limitations

--- Parachute/Parachute/My Project/Application.Designer.vb
+++ Parachute/Parachute/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Parachute.Form1
+            Me.MainForm = Global.Parachute.screenMain
         End Sub
     End Class
 End Namespace
---
+++ Parachute/Parachute/screenMain.vb
@@ -0,0 +1,27 @@
+﻿Public Class screenMain
+
+    Private Sub screenMain_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        'Left/Right controls
+        If e.KeyCode = Keys.D Or e.KeyCode = Keys.Right Then
+            'Check that it's on screen
+            If Character.Left < Me.Width - 14 Then
+                Character.Left = Character.Left + 2
+            End If
+        ElseIf e.KeyCode = Keys.A Or e.KeyCode = Keys.Left Then
+            'Check that it's on screen
+            If Character.Left > 0 Then
+                Character.Left = Character.Left - 2
+            End If
+        End If
+    End Sub
+
+    Private Sub screenMain_Load(sender As Object, e As EventArgs) Handles Me.Load
+        timeScroll.Enabled = True
+    End Sub
+    'This sub will handle object scrolling,
+    'as I imagine it'll be better to have all the enemies scroll
+    'rather than the player, as otherwise it'll be too short
+    Private Sub timeScroll_Tick(sender As Object, e As EventArgs) Handles timeScroll.Tick
+        Character.Top = Character.Top + 2
+    End Sub
+End Class
